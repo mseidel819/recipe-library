da29c0f32fe264997225ea3987a6885500771bc4
Matt Seidel <mseidel819@gmail.com>
2023-12-06 13:20:35 -0600

    feat: add preview data, back button

--- src/app/[...recipe]/page.jsx
+++ src/app/[...recipe]/page.jsx
@@ -1,8 +1,10 @@
 "use client";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import CssBaseline from "@mui/material/CssBaseline";
 import { useEffect, useState, useMemo } from "react";
+import Link from "next/link";
 import BreadData from "../../../data/sallys-baking-addiction/bread.json";
 import BreakfastData from "../../../data/sallys-baking-addiction/breakfast-treats.json";
 import CakeData from "../../../data/sallys-baking-addiction/desserts-cakes.json";
@@ -45,7 +47,41 @@ const Recipe = ({ params }) => {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <div className={styles.container}>
+        <Link className={styles.back_btn} href="/">
+          <ArrowBackIcon />
+          Back
+        </Link>
+
         <h1 className={styles.title}>{recipe.title}</h1>
+        <a className={styles.link} href={recipe.link}>
+          ({recipe.link})
+        </a>
+        <div className={styles.info_container}>
+          <div className={styles.scores}>
+            <span>
+              <strong>Rating: </strong>
+              {recipe.rating}/5 (out of {recipe.num_reviews} reviews)
+            </span>
+            <span>
+              <strong>Servings: </strong>
+              {recipe.serves}
+            </span>
+          </div>
+          <div className={styles.times}>
+            <span>
+              <strong>Prep time: </strong>
+              {recipe.prep_time}
+            </span>
+            <span>
+              <strong>Cook time: </strong>
+              {recipe.cook_time}
+            </span>
+            <span>
+              <strong>Total time: </strong>
+              {recipe.total_time}
+            </span>
+          </div>
+        </div>
         <AccordionComponent recipe={recipe} />
       </div>
     </ThemeProvider>
